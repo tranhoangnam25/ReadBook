c99e0718f707f643df9b02840a8acbd6b7d7c1bc
hoangluyen <hoangluyen23072005k@gmail.com>
2026-03-20 01:15:40 +0700

    Implement CategoryController to handle category requests

---
+++ backend/src/main/java/backend/controller/CategoryController.java
@@ -0,0 +1,21 @@
+package backend.controller;
+
+import backend.entity.Category;
+import backend.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    @GetMapping
+    public List<Category> getAll() {
+        return categoryRepository.findAll();
+    }
+}
