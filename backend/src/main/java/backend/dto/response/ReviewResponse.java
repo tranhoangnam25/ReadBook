a3e421698b0ccdfb9ca19c8f938ba2afab608ad5
hoangluyen <hoangluyen23072005k@gmail.com>
2026-04-04 00:59:00 +0700

    Add ReviewResponse class to handle review data

--- backend/src/main/java/backend/dto/response/ReviewResponse.java
+++ backend/src/main/java/backend/dto/response/ReviewResponse.java
@@ -1,5 +1,24 @@
 package backend.dto.response;
+import backend.entity.*;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReviewResponse {
+    public Long id;
+    public BigDecimal rating;
+    public String comment;
+    public Long userId;
 
+    public ReviewResponse(Review r) {
+        this.id = r.getId();
+        this.rating = r.getRating();
+        this.comment = r.getComment();
+        this.userId = r.getUser().getId();
+    }
 }
+
