54ecb21a88f1d35c378a27e0f79058db57e3b553
hoangluyen <hoangluyen23072005k@gmail.com>
2026-04-05 00:08:08 +0700

    Add files via upload

---
+++ backend/src/main/java/backend/repository/ReviewRepository.java
@@ -0,0 +1,11 @@
+package backend.repository;
+
+import backend.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+
+    List<Review> findByBookId(Long bookId);
+}
