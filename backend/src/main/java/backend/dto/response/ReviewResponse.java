62f14234dc4d93390d726d053928ae15b1130837
hoangluyen <hoangluyen23072005k@gmail.com>
2026-05-07 16:21:13 +0700

    Update ReviewResponse.java

--- backend/src/main/java/backend/dto/response/ReviewResponse.java
+++ backend/src/main/java/backend/dto/response/ReviewResponse.java
@@ -1,24 +1,26 @@
 package backend.dto.response;
-import backend.entity.*;
-import jakarta.persistence.*;
-import lombok.*;
 
+import backend.entity.Review;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ReviewResponse {
+
     public Long id;
     public BigDecimal rating;
     public String comment;
+
     public Long userId;
+    public String username;
+    public String fullName;
 
     public ReviewResponse(Review r) {
         this.id = r.getId();
         this.rating = r.getRating();
         this.comment = r.getComment();
-        this.userId = r.getUser().getId();
+
+        if (r.getUser() != null) {
+            this.userId = r.getUser().getId();
+            this.username = r.getUser().getUsername();
+        }
     }
 }
-
